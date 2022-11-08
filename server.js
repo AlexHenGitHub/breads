08105750d985e092ad3429f9be813ef3a34676bf
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2022-11-07 23:34:33 -0500

    sending index and show data

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-
+ 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads')
@@ -20,6 +21,12 @@ app.get('/', (req, res) => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 
 
 // LISTEN
