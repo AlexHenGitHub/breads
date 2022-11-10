a9c2ecc94cdd20df6cbdddac49bf61ab04f8cbd5
AlexHenGitHub <108377905+AlexHenGitHub@users.noreply.github.com>
2022-11-10 18:55:51 -0500

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,6 +2,8 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+
+
 // INDEX
 breads.get('/', (req, res) => {
     res.render('Index',
@@ -40,13 +42,20 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.send('404')
+    res.render('404')
   }
 })
 
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
 
 
 
